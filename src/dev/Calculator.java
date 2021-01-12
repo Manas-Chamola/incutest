450b00df53bd7b2c9fd9c4fca9efb0abb83346d8
Manas <chamolamanas19@gmail.com>
2021-01-13 02:23:58 +0530

    New Line Delimiter test

--- src/dev/Calculator.java
+++ src/dev/Calculator.java
@@ -3,22 +3,45 @@ package dev;
 public class Calculator {
 
     public static int add (String str){
-        if(str.equals(""))
-        return 0;
+        if(str.equals("")) {
+            return 0;
+        }
+        else if(str.contains("\n")){
+            String nums=extractInt(str);
+            String data[]=nums.split(" ");
+            return sumNumbers(data);
+        }
         else if(str.contains(",")){
             String[] vals =str.split(",");
             if(vals.length==2) {
                 return Integer.parseInt(vals[0]) + Integer.parseInt(vals[1]);
             }
             else{
-                int total=0;
-                for(int i=0; i<vals.length; i++)
-                    total+=Integer.parseInt(vals[i]);
-                return total;
+                return sumNumbers(vals);
             }
         }
-        else
-        return Integer.parseInt(str);
+        else {
+            return Integer.parseInt(str);
+        }
+    }
+
+    static String extractInt(String str)
+    {
+        str = str.replaceAll("[^\\d]", " ");
+        str = str.trim();
+        str = str.replaceAll(" +", " ");
+
+        if (str.equals(""))
+            return "-1";
+
+        return str;
+    }
+
+    static int sumNumbers(String [] vals){
+        int total=0;
+        for(int i=0; i<vals.length; i++)
+            total+=Integer.parseInt(vals[i]);
+        return total;
     }
 
 }
--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -26,4 +26,9 @@ public class CalculatorTest {
     public void returnSumOfMultipleNumbersIfSeparatedByCommas(){
         assertEquals(29, Calculator.add("1,7,10,11"));
     }
+
+    @Test
+    public void acceptNewLineAsDelimiter(){
+        assertEquals(6, Calculator.add("1\n2,3"));
+    }
 }
